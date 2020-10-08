824b06b798dca574bc7f93e6f2d4a83f52014abf
y-tsai <yunchen224@gmail.com>
2020-10-08 13:47:51 +0800

    字串帶入PREVIEW頁面，已可成功列印

--- JS/preview.js
+++ JS/preview.js
@@ -1,9 +1,13 @@
-
+function get_string(){ 
 
 var getUrlString = location.href;
 var url = new URL(getUrlString);
-url.searchParams.get('power');
-url.searchParams.get('type');
-url.searchParams.get('cool_kw');
-url.searchParams.get('cspf');
-url.searchParams.get('cert_no');
+
+document.getElementById('power').innerText = url.searchParams.get('power');
+document.getElementById('Type').innerText = url.searchParams.get('type');
+document.getElementById('cool_kw').innerText = url.searchParams.get('cool_kw');
+document.getElementById('cspf').innerText = url.searchParams.get('cspf');
+document.getElementById('cert_no').innerText = url.searchParams.get('cert_no');
+
+}
+get_string();
--- JS/print_btn.js
+++ JS/print_btn.js
@@ -1,11 +1,13 @@
 function print_btn(value, row){
   return `<a href="EnergyPreview.html?power=${row.power}&type=${row.type}&cool_kw=${row.cool_kw}&cspf=${row.cspf}&cert_no=${row.cert_no}">print</a>`;
-  
+} 
+
+
+function open_pre(click){
+  print_btn.onClick = window.open("EnergyPreview.html","_blank");
 }
 
-function open_pre(){
-  print_btn.onclick = window.open('EnergyPreview.html');
-}
+
 
 var $table = $('#table')
 
