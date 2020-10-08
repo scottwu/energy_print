2388ac197308c73b3f92a0f542d8b64d20f0a29e
y-tsai <yunchen224@gmail.com>
2020-10-08 14:38:07 +0800

    撰寫完成待測試

--- JS/print_btn.js
+++ JS/print_btn.js
@@ -1,16 +1,9 @@
 function print_btn(value, row){
-  return `<a href="EnergyPreview.html?power=${row.power}&type=${row.type}&cool_kw=${row.cool_kw}&cspf=${row.cspf}&cert_no=${row.cert_no}">print</a>`;
+  return `<a target="_blank" href="EnergyPreview.html?power=${row.power}&type=${row.type}&cool_kw=${row.cool_kw}&cspf=${row.cspf}&cert_no=${row.cert_no}">print</a>`;
 } 
-
-
-function open_pre(click){
-  print_btn.onClick = window.open("EnergyPreview.html","_blank");
-}
-
-
 
 var $table = $('#table')
 
   $(function() {
-    $table.bootstrapTable('hideColumn', ('cool_kw','cert_no','power'))
+    $table.bootstrapTable('hideColumn', ['cool_kw','cert_no','power']);
   })
